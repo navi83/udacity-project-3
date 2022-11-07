9a0d2a1fc3200e2961e8a768755cf3daf1460b75
navi83 <hhoangvy.arc@gmail.com>
2022-11-07 13:28:27 +0700

    trigger travis

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aea299d76c14e491692b13352264a374-2023900454.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
