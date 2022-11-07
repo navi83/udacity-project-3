5e39a4698bb96b4aa1ed48d033f3cffc8e232253
navi83 <hhoangvy.arc@gmail.com>
2022-11-07 13:57:08 +0700

    fix config

--- udagram-api-user/src/config/config.ts
+++ udagram-api-user/src/config/config.ts
@@ -7,7 +7,7 @@ export const config = {
   'aws_region': 'us-east-1',
   'aws_profile': 'default',
   'aws_media_bucket': 'project3-748012026617-dev',
-  'url': 'http://a38d73007e0854029b32442335515273-839905590.us-east-1.elb.amazonaws.com:8080',
+  'url': 'http://a38d73007e0854029b32442335515273-839905590.us-east-1.elb.amazonaws.com:8100',
   'jwt': {
     'secret': 'testing',
   },
